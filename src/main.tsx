dfc05ed782b0ca385c54af1e530a41c659ac9c6a
mohamedkhalid725-dot <mohamedkhalid725@gmail.com>
2026-09-14 23:48:18 +0300

    Install account-scoped Firestore sync bridge

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import {installPdfDownloadBridge} from './services/pdfDownloadBridge';
+import {installCloudSyncBridge} from './services/cloudSyncBridge';
 
 installPdfDownloadBridge();
+installCloudSyncBridge();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
