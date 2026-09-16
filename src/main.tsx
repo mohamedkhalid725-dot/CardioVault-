000dab2d1dbd155ec1d6f5ac1899fbcf96d0e539
mohamedkhalid725-dot <mohamedkhalid725@gmail.com>
2026-09-16 21:02:18 +0300

    feat: enable Arabic shaping in generated PDFs

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import './services/pdfArabicSupport';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 import './index.css';
-import {installPdfDownloadBridge} from './services/pdfDownloadBridge';
-import {installCloudSyncBridge} from './services/cloudSyncBridge';
-
-installPdfDownloadBridge();
-installCloudSyncBridge();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
