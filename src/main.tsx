c6c6ebba09e08c4a5c016fafba0ed8d155f11626
mohamedkhalid725-dot <mohamedkhalid725@gmail.com>
2026-09-17 21:27:22 +0300

    Enable native PDF saving and bed reconciliation

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import './services/pdfArabicSupport';
+import './services/bedReconciliation';
+import {installPdfDownloadBridge} from './services/pdfDownloadBridge';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+installPdfDownloadBridge();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
